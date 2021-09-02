3a18a159d9d2081962e545821000ce60b6940542
Diego Garcia <diego_9696c@hotmail.com>
2021-09-02 22:59:01 +0200

    UsersSeeder, PostsSeeder and LikesPostsUsersSeeder created.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -13,6 +15,12 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $users = array(
+            ['name' => 'Diego Garcia', 'email' => 'diego@gmail.com', 'password' => Hash::make('diego'),'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
+            ['name' => 'Jennifer Pich', 'email' => 'jenni@gmail.com', 'password' => Hash::make('jenni'),'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
+            ['name' => 'Albert Aroca', 'email' => 'albert@gmail.com', 'password' => Hash::make('albert'),'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
+        );
+
+        DB::table('users')->insert($users);
     }
 }
